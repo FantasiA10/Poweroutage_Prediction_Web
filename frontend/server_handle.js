f256fa0d145298ba0ba2a79f7353d3e266b68fdf
FantasiA10 <92002690+FantasiA10@users.noreply.github.com>
2023-08-04 03:16:17 -0400

    Update server_handle.js

--- frontend/server_handle.js
+++ frontend/server_handle.js
@@ -1,4 +1,4 @@
-const handleRoute = require('./routes');
+const handleRoute = require('./src/routes');
 const querystring = require('querystring');
 const http = require('http');
 const fs = require('fs');
